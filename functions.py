6f1f6acc6735e635f68299e68c1be38035d4be0a
tmaurer <thamin.maurer@epfl.ch>
2023-10-19 13:58:22 +0200

    Removed __pycache__ from tracking and worked on the different methods for the prediction model

--- functions.py
+++ functions.py
@@ -45,8 +45,36 @@ def compute_loss_logistic(y, tx, w):
 def compute_gradient_logistic(y, tx, w):
     """compute the gradient of loss."""
     pred = tx.dot(w)
-    gradient = tx.T.dot(sigmoid(pred) - y)
+    gradient = tx.T.dot(compute_sigmoid(pred) - y)
     return gradient
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    
+    data_size = len(y)
+    
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+        
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+        
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        
+        yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
@@ -99,7 +127,7 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     return w, loss
 
 
-def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+def reg_logistic_regression(y, tx, initial_w, max_iters, gamma, lambda_):
     """implement regularized logistic regression."""
     w = initial_w
     for n_iter in range(max_iters):
