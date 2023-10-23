155d4309675f2742f7b1fe6f99bedbbe6df6469c
huberamo <huberamo@student.ethz.ch>
2023-10-23 19:25:55 +0200

    update implementations.py

--- implementations.py
+++ implementations.py
@@ -1,19 +1,19 @@
 import functions
 
 def mean_squared_error_gd(y, tx, w, max_iters, gamma):
-    return functions.mean_squared_error_gd
+    return functions.mean_squared_error_gd(y, tx, w, max_iters, gamma)
 
 def mean_squared_error_sgd(y, tx, w, max_iters, gamma,  batch_size=128, num_batches=16): 
-    return functions.mean_squared_error_sgd
+    return functions.mean_squared_error_sgd(y, tx, w, max_iters, gamma,  batch_size=128, num_batches=16)
 
 def least_squares(y, tx): 
-    return functions.least_squares
+    return functions.least_squares(y, tx)
 
 def ridge_regression(y, tx, lambda_):
-    return functions.ridge_regression
+    return functions.ridge_regression(y, tx, lambda_)
 
 def logistic_regression(y, tx, w, max_iters, gamma):
-    return functions.logistic_regression
+    return functions.logistic_regression(y, tx, w, max_iters, gamma)
 
 def reg_logistic_regression(y, tx, w, max_iters, gamma, lambda_): 
-    return functions.reg_logistic_regression
+    return functions.reg_logistic_regression(y, tx, w, max_iters, gamma, lambda_)
