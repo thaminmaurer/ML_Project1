13be1f860bf688b3333b5d144112862fa4e197d2
huberamo <huberamo@student.ethz.ch>
2023-10-26 12:07:35 +0200

    trying to fix logistic_regression

--- implementations.py
+++ implementations.py
@@ -169,12 +169,10 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     """implement logistic regression."""
     w = initial_w
     loss = compute_loss_logistic_minusone_one(y, tx, w)
-    loss = compute_loss_logistic(y, tx, w)
     for n_iter in range(max_iters):
         gradient = compute_gradient_logistic_minusone_one(y, tx, w)
         w = w - gamma * gradient
         loss = compute_loss_logistic_minusone_one(y, tx, w)
-        loss = compute_loss_logistic(y, tx, w)
     return w, loss
 
 
